d10b6e57b21cca5f7a57ccde574075b7928c0810
ChengYi He <chengyihetaipei@gmail.com>
2015-09-14 14:54:34 +0800

    bootimg_updater: support update signature
    bootimg_updater filters out the signatures
    appended to end of boot.img. This commit preserves
    the signatures. It also supports updating
    signatures.

--- bootimg_updater.py
+++ bootimg_updater.py
@@ -40,17 +40,23 @@ class boot_img_hdr:
 
 def read_args(argv):
     parser = argparse.ArgumentParser(description='bootimg updater')
-    parser.add_argument('--bootimg', help='path to the bootimg',  type=argparse.FileType('rb'), required=True)
-    parser.add_argument('--kernel',  help='path to the kernel',   type=argparse.FileType('rb'), default=None)
-    parser.add_argument('--ramdisk', help='path to the ramdisk',  type=argparse.FileType('rb'), default=None)
-    parser.add_argument('--second',  help='path to the second',   type=argparse.FileType('rb'), default=None)
-    parser.add_argument('--dt',      help='path to the dt',       type=argparse.FileType('rb'), default=None)
-    parser.add_argument('--output',  help='path to the output',   type=argparse.FileType('wb'), required=True)
+    parser.add_argument('--bootimg',   help='path to the bootimg',   type=argparse.FileType('rb'), required=True)
+    parser.add_argument('--kernel',    help='path to the kernel',    type=argparse.FileType('rb'), default=None)
+    parser.add_argument('--ramdisk',   help='path to the ramdisk',   type=argparse.FileType('rb'), default=None)
+    parser.add_argument('--second',    help='path to the second',    type=argparse.FileType('rb'), default=None)
+    parser.add_argument('--dt',        help='path to the dt',        type=argparse.FileType('rb'), default=None)
+    parser.add_argument('--signature', help='path to the signagure', type=argparse.FileType('rb'), default=None)
+    parser.add_argument('--output',    help='path to the output',    type=argparse.FileType('wb'), required=True)
     return parser.parse_args(argv)
 
 def pad_file(f, padding):
     pad = (padding - (f.tell() & (padding - 1))) & (padding - 1)
     f.write(struct.pack(str(pad) + 'x'))
+
+def write_buf_data(f, buf):
+    if len(buf) == 0:
+        return
+    f.write(buf)
 
 def write_padded_buf_data(f, buf, padding):
     if len(buf) == 0:
@@ -76,16 +82,20 @@ def write_data(args, header):
     num_kernel_pages = (header.kernel_size + header.page_size - 1) / header.page_size
     num_ramdisk_pages = (header.ramdisk_size + header.page_size - 1) / header.page_size
     num_second_pages = (header.second_size + header.page_size - 1) / header.page_size
+    num_dt_pages = (header.unused[0] + header.page_size - 1) / header.page_size
 
     start_kernel_addr = header.page_size
     start_ramdisk_addr = start_kernel_addr + header.page_size * num_kernel_pages
     start_second_addr = start_ramdisk_addr + header.page_size * num_ramdisk_pages
     start_dt_addr = start_second_addr + header.page_size * num_second_pages
+    start_signature_addr = start_dt_addr + header.page_size * num_dt_pages
+    signature_buf_len = os.fstat(args.bootimg.fileno()).st_size - start_signature_addr
 
     kernel_buf = read_buf_data(args.kernel, args.bootimg, start_kernel_addr, header.kernel_size)
     ramdisk_buf = read_buf_data(args.ramdisk, args.bootimg, start_ramdisk_addr, header.ramdisk_size)
     second_buf = read_buf_data(args.second, args.bootimg, start_second_addr, header.second_size)
     dt_buf = read_buf_data(args.dt, args.bootimg, start_dt_addr, header.unused[0])
+    signature_buf = read_buf_data(args.signature, args.bootimg, start_signature_addr, signature_buf_len)
 
     header.update_image(kernel_buf, ramdisk_buf, second_buf, dt_buf)
 
@@ -94,6 +104,7 @@ def write_data(args, header):
     write_padded_buf_data(args.output, ramdisk_buf, header.page_size)
     write_padded_buf_data(args.output, second_buf, header.page_size)
     write_padded_buf_data(args.output, dt_buf, header.page_size)
+    write_buf_data(args.output, signature_buf)
 
 def main(argv):
     args = read_args(argv)
